89f3fe87144c662bb898e63191958a4d488ba7e7
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2017-04-29 17:49:15 -0500

    Se cambia IP y PORT por defecto

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "IP" : '127.0.0.1' || process.env.IP,
-    "PORT" : 3000 || process.env.PORT,
+    "IP" :  process.env.IP || '127.0.0.1',
+    "PORT" : process.env.PORT || 3000,
     "color_theme" : {
         "info" : "rainbow",
         "data" : "green",
