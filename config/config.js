4cc36cd2a82461c8b4b6acab7db9c17d4a47fa86
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2017-04-30 12:36:32 -0500

    IP se establece en 0.0.0.0

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "IP" :  process.env.IP || '127.0.0.1',
+    "IP" :  process.env.IP || '0.0.0.0',
     "PORT" : process.env.PORT || 3000,
     "color_theme" : {
         "info" : "rainbow",
